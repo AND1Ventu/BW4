130664f3c115f69b17381e5b97ce0ee5e7d56249
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-29 19:38:25 +0100

    creazione dei construttori e dei getter dei setter

--- src/main/java/entities/Manutenzione.java
+++ src/main/java/entities/Manutenzione.java
@@ -19,4 +19,46 @@ public class Manutenzione {
 
     @ManyToOne(mappedBy= "manutenzioneFk")
     private List<Mezzo> mezzi;
+
+    public Manutenzione() {
+    }
+
+    public Manutenzione(Long idManutenzione, LocalDateTime dataInizio, LocalDateTime dataFine, List<Mezzo> mezzi) {
+        this.idManutenzione = idManutenzione;
+        this.dataInizio = dataInizio;
+        this.dataFine = dataFine;
+        this.mezzi = mezzi;
+    }
+
+    public Long getIdManutenzione() {
+        return idManutenzione;
+    }
+
+    public void setIdManutenzione(Long idManutenzione) {
+        this.idManutenzione = idManutenzione;
+    }
+
+    public LocalDateTime getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(LocalDateTime dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public LocalDateTime getDataFine() {
+        return dataFine;
+    }
+
+    public void setDataFine(LocalDateTime dataFine) {
+        this.dataFine = dataFine;
+    }
+
+    public List<Mezzo> getMezzi() {
+        return mezzi;
+    }
+
+    public void setMezzi(List<Mezzo> mezzi) {
+        this.mezzi = mezzi;
+    }
 }
