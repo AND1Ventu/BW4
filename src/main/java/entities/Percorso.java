99d5a343e99f8594d9a5df5a0bce4414de90cf3e
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-29 16:46:04 +0100

    in

--- src/main/java/entities/Percorso.java
+++ src/main/java/entities/Percorso.java
@@ -1,5 +1,7 @@
 package entities;
 
+import javax.persistence.*;
+
 @Entity
 public class Percorso {
 
--- src/main/java/entities/RivenditoreAutorizzato.java
+++ src/main/java/entities/RivenditoreAutorizzato.java
@@ -2,10 +2,14 @@ package entities;
 
 import javax.persistence.*;
 
+import javax.persistence.*;
+import java.util.List;
+
 @Entity
 public class RivenditoreAutorizzato {
 
     @Id
+    @Column(name = "id_distributore")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDistributore;
 
