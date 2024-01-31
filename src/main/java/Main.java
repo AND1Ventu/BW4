15d0bdc4e1b8417c7041b0922c2dab5ba08ed1d1
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2024-01-31 17:57:37 +0100

    last push for today

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,36 +33,23 @@ public class Main {
 //            distributoreDAO.saveDistributore(distributore);
 
             //ticket
-//            Ticket ticket = new Ticket();
-//            ticket.setDataVendita(LocalDateTime.now());
-//            ticket.setTipologia(Ticket.Tipologia.ABBONAMENTO);
-//            ticket.setValidita(Ticket.Validita.WEEK);
-//            ticket.setRivenditoreAutorizzato(distributore);
-//
-//            ticketDAO.aggiungiTicket(ticket);
+//          Ticket ticket = new Ticket();
+//          ticket.setDataVendita(LocalDateTime.of(2024,06,01,0,0));
+//          ticket.setTipologia(Ticket.Tipologia.ABBONAMENTO);
+//          ticket.setValidita(Ticket.Validita.MONTH);
+//          ticket.setDataAttivazione(LocalDateTime.of(2024,06,01,0,0));
+//          ticket.setRivenditoreAutorizzato(distributoreDAO.getDistributoreById(64L));
+
+//          ticketDAO.aggiungiTicket(ticket);
 
 
             //utente
-            Utente utente = new Utente();
-            utente.setNome("Mario");
-            utente.setCognome("Rossi");
-            utente.setTicket(ticketDAO.getById(55L));
-
-            utenteDAO.aggiungiUtente(utente);
-
-            //mezzo
-//            Mezzo mezzo = new Mezzo();
-//            mezzo.setCapienza(55);
-//            mezzo.setInServizio(true);
+//            Utente utente = new Utente();
+//            utente.setNome("Mario");
+//            utente.setCognome("Rossi");
+//            utente.setTicket(ticketDAO.getById(55L));
 //
-//            mezzoDAO.aggiungiMezzo(mezzo);
-
-            //manutenzione
-//            Manutenzione manutenzione = new Manutenzione();
-//            manutenzione.setDataInizio(LocalDateTime.of(2023,12,01,0,0));
-//            manutenzione.setDataFine(LocalDateTime.of(2023,12,30,0,0));
-//
-//            manutenzioneDAO.aggiungiManutenzione(manutenzione);
+//            utenteDAO.aggiungiUtente(utente);
 
             //percorso
 //            Percorso percorso = new Percorso();
@@ -73,9 +60,55 @@ public class Main {
             //tratta
 //            Tratta tratta = new Tratta();
 //            tratta.setDataOraInizioTratta(LocalDateTime.of(2023,12,01,06,30));
-//            tratta.setDataOraFineTratta(LocalDateTime.of(2023,12,01,07,30));
+//            tratta.setDataOraFineTratta(LocalDateTime.of(2023,12,01,8,30));
+//            tratta.setPercorso(percorsoDAO.getPercorsoById(33L));
 //
 //            trattaDAO.aggiungiTratta(tratta);
+
+            //mezzo
+//            Mezzo mezzo = new Mezzo();
+//            mezzo.setCapienza(55);
+//            mezzo.setInServizio(true);
+//            mezzo.setTipologia(Mezzo.Tipologia.AUTOBUS);
+//            mezzo.setPercorso(percorsoDAO.getPercorsoById(33L));
+
+//            mezzoDAO.aggiungiMezzo(mezzo);
+
+            //creazione ticket collegato ad un mezzo
+
+            Mezzo risultato = mezzoDAO.getMezzoById(34L);
+            List<Mezzo> listaMezzi = new ArrayList<>();
+
+            listaMezzi.add(risultato);
+
+
+//        Ticket ticket = ticketDAO.getById(59L);
+//        ticket.setDataVendita(LocalDateTime.of(2024,06,01,0,0));
+//        ticket.setTipologia(Ticket.Tipologia.ABBONAMENTO);
+//        ticket.setValidita(Ticket.Validita.MONTH);
+//        ticket.setDataAttivazione(LocalDateTime.of(2024,06,01,0,0));
+//        ticket.setRivenditoreAutorizzato(distributoreDAO.getDistributoreById(64L));
+//        ticket.setMezzi(listaMezzi);
+//
+//        ticketDAO.aggiungiTicket(ticket);
+
+            //manutenzione
+//            Manutenzione manutenzione = new Manutenzione();
+//            manutenzione.setDataInizio(LocalDateTime.of(2024,01,8,0,0));
+//            manutenzione.setDataFine(LocalDateTime.of(2024,01,10,0,0));
+//            manutenzione.setMezzo(mezzoDAO.getMezzoById(35L));
+//
+//            manutenzioneDAO.aggiungiManutenzione(manutenzione);
+
+
+            //mezzo in manutenzione
+//            Mezzo mezzo = new Mezzo();
+//            mezzo.setCapienza(55);
+//            Mezzo mezzo = mezzoDAO.getMezzoById(35L);
+//            mezzo.setInServizio(false);
+//            mezzo.setTipologia(Mezzo.Tipologia.AUTOBUS);
+//            mezzo.setPercorso(percorsoDAO.getPercorsoById(33L));
+//            mezzoDAO.aggiungiMezzo(mezzo);
 
 
         } catch (Exception e) {
@@ -84,9 +117,25 @@ public class Main {
             }
             e.printStackTrace();
         } finally {
-            ticketDAO.closeEntityManager(em);
+//            ticketDAO.closeEntityManager(em);
         }
 
+        //numero biglietti venduti da un distributore in un range temporale
+        System.out.println(distributoreDAO.getTicketPuntoEmissioneById(63L,LocalDateTime.of(2024,01,01,0,0),LocalDateTime.of(2024,02,28,0,0)));
+
+        //verifica validità abbonamento
+        ticketDAO.ricercaTicketPerId(59L);
+
+        //numri biglietti vidimati su un mezzo
+        System.out.println(mezzoDAO.numeroBigliettiMezzo(34L));
+
+        //numero biglietti vidimati in un periodo di tempo
+        System.out.println(ticketDAO.attivatiPerDate(LocalDateTime.of(2024,01,01,0,0),LocalDateTime.of(2024,12,31,0,0)));
+
+        //tenere traccia dello stato di servizio del mezzo
+        System.out.println(mezzoDAO.statoManutenzione(35L));
+
+        //
 
     }
 
